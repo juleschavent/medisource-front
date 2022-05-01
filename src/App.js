7772c2599fb29460954241514693ee32d4e2bc4b
jules <juleschavent@gmail.com>
2022-05-01 19:33:38 +0200

    added env var

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from '@mui/material/Button'
 
 function App() {
+  console.log(process.env)
   return (
     <div className="App" id="app">
       <Button variant="contained" color="secondary">Contained</Button>
