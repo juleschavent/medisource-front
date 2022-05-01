a7facdfe44dfb7b825a93b71f9a2b08102a11ea9
jules <juleschavent@gmail.com>
2022-05-01 12:28:05 +0200

    quick fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Button from '@mui/material/Button'
-import { useTheme } from '@emotion/react'
 
 function App() {
   return (
     <div className="App" id="app">
       <Button variant="contained" color="primary">Contained</Button>
+      test
     </div>
   )
 }
